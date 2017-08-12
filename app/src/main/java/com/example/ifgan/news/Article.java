6655b8ec0d1f0df8fb8fa0d8c23512454cb8d47f
Igor Guilherme <ifgandrade@outlook.com>
2017-08-12 17:37:02 -0300

    first commit

---
+++ app/src/main/java/com/example/ifgan/news/Article.java
@@ -0,0 +1,57 @@
+package com.example.ifgan.news;
+
+/**
+ * Created by ifgan on 12/08/2017.
+ */
+
+public class Article {
+
+    /**
+     * Name of the section
+     */
+    private String Section;
+
+    /**
+     * Name of the article
+     */
+    private String Title;
+
+    /**
+     * Website URL of the article
+     */
+    private String Url;
+
+    /**
+     * Constructs a new (@link Article) object.
+     *
+     * @param section is the section of the article
+     * @param title   is the title of the article
+     * @param url     is the url of the article
+     */
+    public Article(String section, String title, String url) {
+        Section = section;
+        Title = title;
+        Url = url;
+    }
+
+    /**
+     * Returns the section of the article
+     */
+    public String getSection() {
+        return Section;
+    }
+
+    /**
+     * Returns the title of the article
+     */
+    public String getTitle() {
+        return Title;
+    }
+
+    /**
+     * Returns the url of the article
+     */
+    public String getUrl() {
+        return Url;
+    }
+}
---
+++ app/src/main/java/com/example/ifgan/news/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.ifgan.news;
+
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<Article>> {
+
+    /**
+     * URL to query the Guardian dataset for section Tecnology information
+     */
+    private static final String GUARDIAN_REQUEST_URL =
+            "https://content.guardianapis.com/technology?api-key=test";
+
+    /**
+     * Constant value for the article loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int ARTICLE_LOADER_ID = 1;
+
+    /**
+     * Adapter for the list of earthquakes
+     */
+    private ArticleAdapter mAdapter;
+
+    /** TextView that is displayed when the list is empty */
+    private TextView mEmptyStateTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView articleListView = (ListView) findViewById(R.id.list);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        articleListView.setEmptyView(mEmptyStateTextView);
+
+        // Create a new adapter that takes an empty list of articles as input
+        mAdapter = new ArticleAdapter(this, new ArrayList<Article>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        articleListView.setAdapter(mAdapter);
+
+        // Set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website with more information about the selected article.
+        articleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current article that was clicked on
+                Article currentArticle = mAdapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri articleUri = Uri.parse(currentArticle.getUrl());
+
+                // Create a new intent to view the article URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, articleUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            loaderManager.initLoader(ARTICLE_LOADER_ID, null, this);
+        }
+        else
+        {
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+
+    }
+
+
+    @Override
+    public Loader<List<Article>> onCreateLoader(int id, Bundle bundle) {
+        // Create a new loader for the given URL
+        return new ArticleLoader(this, GUARDIAN_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Article>> loader, List<Article> articles) {
+        // Set empty state text to display "No earthquakes found."
+        mEmptyStateTextView.setText(R.string.no_articles);
+
+        // Clear the adapter of previous articles data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (articles != null && !articles.isEmpty()) {
+            mAdapter.addAll(articles);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Article>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
+    }
+}
